3c64f37a7da63293c26dbe64c50cfcd9080110f1
Raiyan109 <kraiyan109@gmail.com>
2023-04-23 19:16:41 +0600

    rejected

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div>
             <Header />
-            <section className="flex items-center h-full p-16 bg-lightYellow p-32">
+            <section className="flex items-center h-full bg-lightYellow p-32">
                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                     <div className="max-w-md text-center">
                         <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
