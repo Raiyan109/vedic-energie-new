fef800a8085755a7cdfdeb9a3477825e34af09b2
pooaras <83764553+pooaras@users.noreply.github.com>
2023-04-27 08:58:48 +0530

    whiteresolved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,8 +23,8 @@ const Home = () => {
             <EnergyResidency />
             <Upcoming />
             <Affiliate />
-            <Client />
-            <Brands />
+            {/* <Client />
+            <Brands /> */}
             <See />
             <Articles />
             <Footer />
