c067b97ef4774adec243d57ac920f35ae75d1b4e
Raiyan109 <kraiyan109@gmail.com>
2023-04-28 11:19:37 +0600

    range style

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const RangeSlider = ({ value, handleRange }) => {
     return (
         <div>
-            <fieldset className="flex flex-nowrap items-center max-w-xs space-x-2 dark:text-gray-100">
+            <fieldset className="flex flex-nowrap items-center max-w-xs space-x-2">
                 <label for="No of people in your home">
-                    <input id="slider" type="range"
+                    <Slider id="slider" type="range"
                         min='1'
                         max='10'
                         defaultValue={value}
                         onChange={handleRange}
-                        className="w-full h-2 rounded-lg cursor-pointer accent-lightYellow" />
+                        className="" />
                 </label>
             </fieldset>
         </div>
@@ -18,3 +19,35 @@ const RangeSlider = ({ value, handleRange }) => {
 };
 
 export default RangeSlider;
+
+const Slider = styled.input`
+-webkit-appearance:none;
+width:100%;
+height:6px;
+border-radius:4px;
+background-color:#FEFFCD;
+outline:none;
+opacity:1;
+--webkit-transition:0.2s;
+transition:opacity 0.2s;
+
+::-webkit-slider-thumb{
+    -webkit-appearance:none;
+    appearance:none;
+    width:16px;
+    height:16px;
+    border-radius:50%;
+    background:#C1F4C8;
+    cursor:pointer;
+}
+
+::-moz-range-thumb{
+    -webkit-appearance:none;
+    appearance:none;
+    width:16px;
+    height:16px;
+    border-radius:50%;
+    background:#C1F4C8;
+    cursor:pointer;
+}
+`
