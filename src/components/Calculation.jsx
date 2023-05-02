5a0502d36052552af6961ae4bff2962b24339a83
Raiyan109 <kraiyan109@gmail.com>
2023-05-02 13:00:30 +0600

    garbage

--- src/components/Calculation.jsx
+++ src/components/Calculation.jsx
@@ -13,19 +13,13 @@ import styled from 'styled-components';
 const Calculation = ({ ref }) => {
     const [peopleRangeValue, setPeopleRangeValue] = useState(5)
     const [unitRangeValue, setUnitRangeValue] = useState(100)
-    const [isOpenState, setIsOpenState] = useState(false)
-    const [isOpenDistrict, setIsOpenDistrict] = useState(false)
-    const [isOpenCity, setIsOpenCity] = useState(false)
-    const [inputValueState, setInputValueState] = useState('')
-    const [inputValueDistrict, setInputValueDistrict] = useState('')
-    const [inputValueCity, setInputValueCity] = useState('')
-    const [selectedState, setSelectedState] = useState('')
-    const [selectedDistrict, setSelectedDistrict] = useState('')
-    const [selectedCity, setSelectedCity] = useState('')
+
 
     const [statesId, setStatesId] = useState('')
     const [district, setDistrict] = useState([])
+    const [city, setCity] = useState([])
     const [districtId, setDistrictId] = useState('')
+    const [cityId, setCityId] = useState('')
 
     const handlePeopleRange = (e) => {
         setPeopleRangeValue(e.target.value)
@@ -38,7 +32,9 @@ const Calculation = ({ ref }) => {
     const handleStates = (e) => {
         const getStateId = e.target.value
         const getDistrictData = statesData.find(state => state.state_id === getStateId)?.districts
+        const getCityData = statesData.find(state => state.state_id === getStateId)?.cities
         setDistrict(getDistrictData)
+        setCity(getCityData)
         setStatesId(getStateId)
         console.log(getStateId);
     }
@@ -47,6 +43,12 @@ const Calculation = ({ ref }) => {
         const districtId = e.target.value
         console.log(districtId);
         setDistrictId(districtId)
+    }
+
+    const handleCities = (e) => {
+        const cityId = e.target.value
+        console.log(cityId);
+        setCityId(cityId)
     }
 
     const handleSubmit = (e) => {
@@ -70,30 +72,9 @@ const Calculation = ({ ref }) => {
                             <h3 className="lg:text-5xl md:text-4xl font-semibold tracking-tight text-3xl text-white py-5">Energy Usage Calculator For Your Home</h3>
 
                             <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-6 gap-7 py-10'>
-
-
-                                {/* <div className='relative block border-b border-green pt-6 focus-within:border-blue'>
-                                    <button
-                                        className='text-lightYellow'
-                                        onClick={() => setIsOpen((prev) => !prev)}
-                                    >Dropdown</button>
-
-                                    {isOpen && (
-                                        <div className='absolute bg-lightYellow top-[50px] flex flex-col items-start rounded-lg p-2 w-full z-10 max-h-60 overflow-y-auto'>
-                                            {states.map((item, index) => (
-                                                <div className='cursor-pointer hover:bg-orange hover:text-black hover:w-full rounded-lg p-1' key={index}>
-                                                    <h3>{item.state}</h3>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )}
-                                </div> */}
-
-                                {/* new dropdown - STATE */}
-
-
                                 <form onSubmit={handleSubmit}>
 
+                                    {/*  STATE */}
                                     <div className="select mb-10">
                                         <select name="state" id="state" onChange={(e) => handleStates(e)}>
                                             <option selected disabled>State</option>
@@ -105,7 +86,8 @@ const Calculation = ({ ref }) => {
                                         </select>
                                     </div>
 
-                                    <div className="select">
+                                    {/* Districts */}
+                                    <div className="select mb-10">
                                         <select name="district" id="district" onChange={(e) => handleDistricts(e)}>
                                             <option selected disabled>District</option>
 
@@ -116,156 +98,29 @@ const Calculation = ({ ref }) => {
                                             }
                                         </select>
                                     </div>
-                                    <button></button>
+
+                                    {/* Cities */}
+                                    <div className="select">
+                                        <select name="city" id="city" onChange={(e) => handleCities(e)}>
+                                            <option selected disabled>
+                                                City
+                                            </option>
+
+                                            {
+                                                city?.map((item, idx) => (
+                                                    <option value={item.city_id} key={idx}>{item.city_name}</option>
+                                                ))
+                                            }
+
+                                        </select>
+                                    </div>
                                 </form>
 
-                                {/* <div className='w-full h-8 font-medium z-10'> */}
-                                {/* <div
-                                        onClick={() => setIsOpenState(!isOpenState)}
-                                        className='w-full mt-8 flex items-center justify-start border-b-2 text-lightYellow'>
-                                        {selectedState
-                                            ? selectedState
-                                            : 'State'}
-                                    </div> */}
-                                {/* <ul className={`bg-lightYellow mt-2 overflow-y-auto text-black ${isOpenState ? 'max-h-60' : 'max-h-0'}`}> */}
-
-                                {/* input field */}
-                                {/* <div className='flex items-center px-2 sticky top-0 bg-lightYellow'>
-                                            <AiOutlineSearch size={18} className='' />
-                                            <input
-                                                type="text"
-                                                value={inputValueState}
-                                                onChange={(e) => setInputValueState(e.target.value.toLowerCase())}
-                                                placeholder='Enter state name'
-                                                className='bg-lightYellow p-2 outline-none focus:outline-none focus:ring focus:ring-lightYellow border-none'
-                                            />
-                                        </div> */}
-                                {/* {statesData.map((item, index) => (
-                                            <li
-                                                key={index}
-                                                className={`p-2 text-sm cursor-pointer hover:bg-yellow hover:text-black 
-                                                ${item.state_name.toLowerCase() === selectedState.toLowerCase() && 'bg-yellow text-black'}
-                                                ${item.state_name.toLowerCase().startsWith(inputValueState) ? 'block'
-                                                        :
-                                                        'hidden'
-                                                    }`}
-                                                onClick={() => {
-                                                    if (item.state_name.toLowerCase() !== selectedState.toLowerCase()) {
-                                                        setSelectedState(item.state_name)
-                                                        setIsOpenState(false)
-                                                        setInputValueState('')
-                                                    }
-                                                }}
-                                            >
-                                                {item.state_name}
-                                            </li>
-                                        ))} */}
-                                {/* </ul>
-                                </div> */}
-
-                                {/*  */}
-
-                                {/* new dropdown - DISTRICT */}
-
-                                {/* <div className='w-full h-8 font-medium z-10'> */}
-                                {/* <div
-                                        onClick={() => setIsOpenDistrict(!isOpenDistrict)}
-                                        className='w-full mt-8 flex items-center justify-start border-b-2 text-lightYellow'>
-                                        {selectedDistrict
-                                            ? selectedDistrict
-                                            : 'District'}
-                                    </div> */}
-                                {/* <ul className={`bg-lightYellow mt-2 overflow-y-auto text-black ${isOpenDistrict ? 'max-h-60' : 'max-h-0'}`}> */}
-
-                                {/* input field */}
-                                {/* <div className='flex items-center px-2 sticky top-0 bg-lightYellow'>
-                                            <AiOutlineSearch size={18} className='' />
-                                            <input
-                                                type="text"
-                                                value={inputValueDistrict}
-                                                onChange={(e) => setInputValueDistrict(e.target.value.toLowerCase())}
-                                                placeholder='Enter state name'
-                                                className='bg-lightYellow p-2 outline-none focus:outline-none focus:ring focus:ring-lightYellow border-none'
-                                            />
-                                        </div> */}
-                                {/* {districts.map((item, index) => (
-                                            <li
-                                                key={index}
-                                                className={`p-2 text-sm cursor-pointer hover:bg-yellow hover:text-black 
-                                                ${item.name.toLowerCase() === selectedDistrict.toLowerCase() && 'bg-yellow text-black'}
-                                                ${item.name.toLowerCase().startsWith(inputValueDistrict) ? 'block'
-                                                        :
-                                                        'hidden'
-                                                    }`}
-                                                onClick={() => {
-                                                    if (item.name.toLowerCase() !== selectedDistrict.toLowerCase()) {
-                                                        setSelectedDistrict(item.name)
-                                                        setIsOpenDistrict(false)
-                                                        setInputValueDistrict('')
-                                                    }
-                                                }}
-                                            >
-                                                {item.name}
-                                            </li>
-                                        ))} */}
-                                {/* </ul>
-                                </div> */}
-
-                                {/*  */}
-
-                                {/* new dropdown - CITY */}
-
-                                {/* <div className='w-full h-8 font-medium z-10'> */}
-                                {/* <div
-                                        onClick={() => setIsOpenCity(!isOpenCity)}
-                                        className='w-full mt-8 flex items-center justify-start border-b-2 text-lightYellow'>
-                                        {selectedCity
-                                            ? selectedCity
-                                            : 'City'}
-                                    </div> */}
-                                {/* <ul className={`bg-lightYellow mt-2 overflow-y-auto text-black ${isOpenCity ? 'max-h-60' : 'max-h-0'}`}> */}
-
-                                {/* input field */}
-                                {/* <div className='flex items-center px-2 sticky top-0 bg-lightYellow'>
-                                            <AiOutlineSearch size={18} className='' />
-                                            <input
-                                                type="text"
-                                                value={inputValueCity}
-                                                onChange={(e) => setInputValueCity(e.target.value.toLowerCase())}
-                                                placeholder='Enter state name'
-                                                className='bg-lightYellow p-2 outline-none focus:outline-none focus:ring focus:ring-lightYellow border-none'
-                                            />
-                                        </div> */}
-                                {/* {cities.map((item, index) => (
-                                            <li
-                                                key={index}
-                                                className={`p-2 text-sm cursor-pointer hover:bg-yellow hover:text-black 
-                                                ${item.name.toLowerCase() === selectedCity.toLowerCase() && 'bg-yellow text-black'}
-                                                ${item.name.toLowerCase().startsWith(inputValueCity) ? 'block'
-                                                        :
-                                                        'hidden'
-                                                    }`}
-                                                onClick={() => {
-                                                    if (item.name.toLowerCase() !== selectedCity.toLowerCase()) {
-                                                        setSelectedCity(item.name)
-                                                        setIsOpenCity(false)
-                                                        setInputValueCity('')
-                                                    }
-                                                }}
-                                            >
-                                                {item.name}
-                                            </li>
-                                        ))} */}
-                                {/* </ul>
-                                </div> */}
-
-                                {/*  */}
-
-                                {/* new dropdown - Billed unit per month */}
+                                {/*  Billed unit per month */}
 
                                 <label
                                     for="BilledUnitsPerMonth"
-                                    className="relative block overflow-hidden border-b-2 border-lightYellow"
+                                    className="relative block overflow-hidden border-b-2 border-lightYellow pt-36 lg:ml-16 ml-0"
                                 >
                                     <input
                                         style={{ background: 'transparent' }}
@@ -277,8 +132,6 @@ const Calculation = ({ ref }) => {
                                 </label>
 
                                 {/*  */}
-
-
 
                                 {/* Slider range people count */}
                                 <PeopleRangeSlider min='1' max={20} value={peopleRangeValue} handlePeopleRange={handlePeopleRange}
@@ -297,12 +150,11 @@ const Calculation = ({ ref }) => {
                                 <div className='pt-6'>
                                     <div className='w-20 h-9 bg-lightYellow flex justify-center items-center text-xl text-rgbaHeader'>{unitRangeValue}</div>
                                 </div>
+
                                 <LetsGoButton />
                             </div>
                         </div>
-
                     </div>
-
                 </div>
             </section>
         </Section>
