f932eb0793a1df2146a74f43417ebc884c61c5a5
Raiyan109 <kraiyan109@gmail.com>
2023-05-11 23:41:00 +0600

    home

--- src/components/Tipscategory.jsx
+++ src/components/Tipscategory.jsx
@@ -6,7 +6,7 @@ export default function Tipscategory() {
 
   return (
     <div className="bg-orange py-[100px]">
-      <h5 className=" h-1 not-italic font-bold text-3xl text-center text-indigo py-[70px]">Tips that can save energy and money
+      <h5 className="h-1 not-italic font-bold text-3xl text-center text-indigo py-[70px]">Tips that can save energy and money
       </h5>
       <details className="group [&_summary::-webkit-details-marker]:hidden border-[3px] bg-white border-brown my-10 mx-10">
 
