f0484d675dca936ac1da5cf83a6672e825f3f530
Raiyan109 <kraiyan109@gmail.com>
2023-06-18 17:08:53 +0600

    banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
 import Header from '../components/Header';
-import Banner from '../components/Banner';
-import Footer from '../components/Footer';
+
 import Efficient from '../components/Efficient';
 import EnergyResidency from '../components/EnergyResidency';
 import Upcoming from '../components/Upcoming';
-import Client from '../components/Client';
 import See from '../components/See';
 import Articles from '../components/Articles';
-import Brands from '../components/Brands';
 import Affiliate from '../components/Affiliate';
 import BackToTopButton from '../components/BackToTopButton';
 import Hero from '../components/Hero';
-import Footer2 from '../components/Footer2';
 import Footer3 from '../components/Footer3';
 
 
