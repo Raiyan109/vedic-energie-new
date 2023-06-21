9b7a4812ceec187b699f256b5530b366dbfde63d
Raiyan109 <kraiyan109@gmail.com>
2023-06-21 13:50:51 +0600

    google analytics universal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes'
 import styled from 'styled-components';
 import CalcProvider from './context/CalcProvider';
-
+import ReactGA from 'react-ga'
+import { useEffect } from 'react';
 
 const Container = styled.div`
 `;
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-EERQNM32EJ');
+    ReactGA.pageview('/');
+  }, [])
   return (
     <CalcProvider>
       <Container>
