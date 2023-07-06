c7b767185b7e0b9275ea0040e710318daaec5015
Raiyan109 <kraiyan109@gmail.com>
2023-07-06 21:48:02 +0500

    modal complete without EMI

---
+++ src/components/EMICalculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EMICalculator = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EMICalculator;
--- src/components/SolarModal.jsx
+++ src/components/SolarModal.jsx
@@ -41,46 +41,55 @@ const SolarModal = ({powerPlantSize,plantCost,electricityGeneration,financialSav
                                 <span>2 .</span>
                                     <h1>Cost of the plant:</h1>
                             </div>
-                            <div className='flex gap-4'>
+                            <div className='flex gap-4 h-16 mb-4'>
                                 <span>3 .</span>
-                                    <h1>Total electricity generation from sdivar plant</h1>
+                                    <div>
+                                        <h1>Total electricity generation from solar plant</h1>
+                                        <h3>Annual :</h3>
+                                        <h3>Life-time (25 years) :</h3>
+                                    </div>
                             </div>
-                            <div className='flex gap-4'>
+                            <div className='flex gap-4 h-16'>
                                 <span>4 .</span>
+                                    <div>
                                     <h1>Financial savings</h1>
+                                    <h3>Monthly :</h3>
+                                    <h3>Annually :</h3>
+                                    <h3>Life-time (25 years) :</h3>
+                                    </div>
                             </div>
-                            <div className='flex gap-4'>
-                                <span>5 .</span>
-                                    <h1>Co2 Mitigated</h1>
+                            <div className='flex gap-4 h-16 mt-9 border-t-2'>
+                                {/* <span>5 .</span> */}
+                                    <div>
+                                      <h1 className='font-bold'>Carbon dioxide emissions mitigated is</h1>
+                                      <h1 className='font-bold'>This installation will be equivalent to planting</h1>
+                                    </div>
                             </div>
                         </div>
                     </div>
                     <div className='bg-gray flex-2'>
-                        <p>{powerPlantSize}</p>
-                        <p>{plantCost}</p>
-                        <p>{electricityGeneration}</p>
-                        <p>{financialSaving}</p>
-                        <p>{co2Mitigated}</p>
-                        <p>{equivalentPlanting}</p>
+                        <p className='mt-5'>{powerPlantSize} kW</p>
+                        <p className='mb-6'>{plantCost} Rs.</p>
+                        <p className=''>{electricityGeneration} kWh</p>
+                        <p className='mb-8'>{`${electricityGeneration * 25} kWh`}</p>
+                        <p>Rs. {financialSaving}</p>
+                        <p>Rs. {`${financialSaving*10}`}</p>
+                        <p>Rs. {`${financialSaving*10*25}`}</p>
+                        <p className='border-t-2 mt-1'>{co2Mitigated} Tonnes</p>
+                        <p>{equivalentPlanting.toFixed()} Teak trees over the life time.</p>
                     </div>
                   </div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-green bg-orange font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
+                  
                 </div>
               </div>
             </div>
