08a3927d05507cb3dff566e97bd0e34dde061809
yash <yashpimplapure@live.com>
2023-10-09 10:34:10 +0530

    shop alignment

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,14 +5,16 @@ import Footer from '../components/Footer';
 const Shop = () => {
     return (
         <div>
-            <Header></Header>
+            <Header/>
            
-           <section className=''>
-           <h1>Hello</h1>
-      
-            </section>
-           
-            <Footer></Footer>
+           <section className='flex items-center h-full  p-32'>
+            <div className='container flex flex-col item-center justify-center'>
+                <h1>hello</h1>
+
+            </div>
+
+           </section>
+            <Footer/>
         </div>
     );
 };
