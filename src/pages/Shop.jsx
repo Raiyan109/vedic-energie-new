604d2e07f2237cd01951c0924b12cd0dedb2cf38
polyakhtar <poly09876pm@gmail.com>
2023-10-31 20:12:27 +0600

    display product in shop component in grid

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,38 +4,40 @@ import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
- const [data, setData] = useState([]);
- useEffect(() => {
-  fetch('https://vedic-energie-server-6c3c7yp8s-polyakhtar.vercel.app/products')
-    .then((res) => res.json())
-    .then((data) => {
-      setData(data);
-    });
- }, []);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://vedic-energie-server-6c3c7yp8s-polyakhtar.vercel.app/products')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+            });
+    }, []);
 
     return (
         <div className=' '>
             <Header />
             <section className='bg-blue mx-auto flex items-center h-full lg:p-32 pb-10 pt-32' >
-                <div className="flex lg:w-2/3 mx-2 lg:mx-auto grid grid-cols-1 gap-6 overflow-hidden rounded-lg shadow-lg">
+                <div className="flex lg:mx-auto mx-auto grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-6 overflow-hidden rounded-lg shadow-lg">
                     {
                         data.map(solar => (
-                            <div key={solar._id} className="flex overflow-hidden bg-white rounded-lg shadow-lg">
-                                <div className="w-1/3 bg-cover" style={{ backgroundImage: `url(${solar.image})`, backgroundSize: '100% 100%' }} />
-                                <div className="lg:w-2/3 w-full p-4 md:p-4">
-                                    <h1 className="text-xl font-bold text-gray-800 my-2">{solar.name}</h1>
-                                    <p className='text-md'>
-                                        {solar.pros}
-                                    </p>
-                                    <p className="text-md font-semibold text-gray-600 my-2">Old Price: {solar.oldPrice}</p>
-                                    <p className="text-md font-semibold text-gray-600 my-2">New Price: {solar.newPrice}</p>
-                                    <Link to={`/shop/${solar._id}`}>
-                                        <button className='bg-blue text-white rounded-md px-2 py-1 text-sm'>READ MORE</button>
-                                    </Link>
+                            <div key={solar._id} className="flex overflow-hidden rounded-lg shadow-lg">
+                                <div className="w-full max-w-xs overflow-hidden  bg-yellow">
+                                    <div className="h-56 relative">
+                                        <img className="object-cover w-full h-full" src={solar.image} alt="avatar" />
+                                    </div>
+                                    <div className="py-5 my-2">
+                                        <a href="#" className="block text-xl font-bold text-center" tabIndex={0} role="link">{solar.name}</a>
+                                        <span className="text-sm text-black font-semibold mx-6">
+                                            <span className='mr-2'>Price: </span>
+                                            <span className='line-through mr-2'>{solar.oldPrice} </span>
+                                            <span className='mr-2'>{solar.newPrice}</span>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         ))
                     }
+
                 </div>
             </section>
             <Footer />
